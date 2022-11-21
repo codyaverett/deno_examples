1dc9f7b04799da0e8167c1f3da40acf9da8f254e
Cody Averett <codyaverett@gmail.com>
2022-11-21 02:59:10 -0600

    expanded on test examples

--- asserts/test.ts
+++ asserts/test.ts
@@ -1,5 +1,5 @@
 import {
-    assert, assertEquals, assertNotEquals, assertThrows, assertRejects,
+    assert, assertEquals, assertNotEquals, assertThrows, assertRejects, assertStrictEquals,
 } from "https://deno.land/std/testing/asserts.ts";
 
 Deno.test("successful asserts", () => {
@@ -46,13 +46,76 @@ Deno.test("assertThrows with custom error", () => {
 });
 
 
-Deno.test("assertThrows with async function", async () => {
+Deno.test("assertRejects within async function or promise", async () => {
     await assertRejects(async () => {
         await new Promise((resolve, reject) => {
             setTimeout(() => {
                 reject("Hello Hell");
                 resolve("never");
-            }, 1000);
+            }, 10);
         });
     });
 });
+
+Deno.test("assertRejects with async function and custom error", async () => {
+    await assertRejects(
+        async () => {
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    reject(new Error("Hello Hell"));
+                    resolve("never");
+                }, 10);
+            });
+        },
+        Error,
+        "Hello Hell"
+    );
+});
+
+Deno.test("assert strict equals", () => {
+    assertStrictEquals("hello", "hello");
+});
+
+// Deno.test("assert strict not equals", () => {
+//     assertStrictEquals("hello", "world");
+// });
+
+Deno.test("assert string contains", () => {
+    assert("hello world".includes("world"));
+});
+
+Deno.test("assert string not contains", () => {
+    assert(!"hello world".includes("deno"));
+});
+
+Deno.test("assert string starts with", () => {
+    assert("hello world".startsWith("hello"));
+});
+
+Deno.test("assert string not starts with", () => {
+    assert(!"hello world".startsWith("world"));
+});
+
+Deno.test("assert string ends with", () => {
+    assert("hello world".endsWith("world"));
+});
+
+Deno.test("assert Array contains", () => {
+    assert(["hello", "world"].includes("world"));
+});
+
+Deno.test("assert Array not contains", () => {
+    assert(!["hello", "world"].includes("deno"));
+});
+
+// Regular expressions
+Deno.test("assert match (regular expressions)", () => {
+    assert("hello world".match(/world/))
+});
+
+// Deno.test("assert file does not exist", async () => {
+//     const file = await Deno.open("test.txt", { create: true });
+//     file.close();
+//     assert(!(await Deno.stat("test.txt")).isFile);
+// });
+
